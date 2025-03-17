7e823e6faeff70eff3a404c5981e44ccdef2991e
Matias <matias.alvare.n@gmail.com>
2025-03-17 20:59:29 +0100

    lifted state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+
 const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
 
-export function GameBoard() {
+export function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+
+  const [gameBoard, setGameBoard] = useState(INITIAL_GAME_BOARD);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard(prevGameBoard => {
+      const updatedBoard = prevGameBoard.map(row => [...row]);
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare()
+  };
+
   return (
     <ol id="game-board">
-      {INITIAL_GAME_BOARD.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
