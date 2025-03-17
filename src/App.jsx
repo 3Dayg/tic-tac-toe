765455c9f02cceb1888428fa7952a43b2731a057
Matias <matias.alvare.n@gmail.com>
2025-03-17 08:47:01 +0100

    added base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
+import Player from './components/Player'
 
 function App() {
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X" />
+        <Player name="Player 2" symbol="O" />
+      </ol>
 
-  return (
-    <>
-      <h1>NEW APP</h1>
-    </>
-  )
-}
+      GAME BOARD
+    </div>
+  </main>
+};
 
 export default App
